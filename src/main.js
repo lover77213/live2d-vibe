984a8de1e6e13401dfe0760d6ab0c18689757414
lover77213 <adultrabbit2213@gmail.com>
2026-05-15 01:22:59 +0800

    fix: remove imports to solve SyntaxError

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 async function init() {
-    // 因為在 index.html 已經載入了 pixi.min.js，這裡直接使用全域變數 PIXI
     const app = new PIXI.Application({
         view: document.getElementById("canvas"),
         autoStart: true,
@@ -7,7 +6,7 @@ async function init() {
         resizeTo: window
     });
 
-    // 使用全域變數 PIXI.live2d (由 pixi-live2d-display 插件提供)
+    // 透過全域變數 PIXI.live2d 存取插件
     const model = await PIXI.live2d.Live2DModel.from("public/model/model.model3.json");
     app.stage.addChild(model);
     
