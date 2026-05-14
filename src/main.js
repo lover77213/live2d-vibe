24e2a1fac79e97b9ad1e1e3620d1239e742746b1
lover77213 <adultrabbit2213@gmail.com>
2026-05-15 01:42:16 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import * as PIXI from 'https://esm.sh/pixi.js@7.2.4';
 import { Live2DModel } from 'https://esm.sh/pixi-live2d-display@0.5.0-beta.7';
 
-// 暴露給全域，讓 CubismCore 使用
+// 暴露給全域，讓 CubismCore 能夠讀取
 window.PIXI = PIXI;
 
 async function init() {
