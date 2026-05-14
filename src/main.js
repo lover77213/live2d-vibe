7ac3796d604800144b17f263af972eafa8685c07
lover77213 <adultrabbit2213@gmail.com>
2026-05-15 01:08:10 +0800

    fix: cleanup paths and imports

--- src/main.js
+++ src/main.js
@@ -8,13 +8,9 @@ async function init() {
         backgroundAlpha: 0,
         resizeTo: window
     });
-
-    // 指向你貼圖與模型所在的正確資料夾
-    const model = await Live2DModel.from("public/model/精細模型.model3.json");
+    const model = await Live2DModel.from("精細模型.model3.json");
     app.stage.addChild(model);
-    
     model.x = 0;
     model.y = 0;
 }
-
 init();
