40bbfa504d554c465bff1c22221c73bca50bf160
lover77213 <adultrabbit2213@gmail.com>
2026-05-15 01:29:46 +0800

    fix: resolve conflicts and clean code

--- src/main.js
+++ src/main.js
@@ -5,11 +5,8 @@ async function init() {
         backgroundAlpha: 0,
         resizeTo: window
     });
-
-    // 透過全域變數 PIXI.live2d 讀取
     const model = await PIXI.live2d.Live2DModel.from("public/model/model.model3.json");
     app.stage.addChild(model);
-    
     model.x = 0;
     model.y = 0;
 }
