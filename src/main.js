9cbbc86719908021f4831dd6b2dfada31cd82be8
lover77213 <adultrabbit2213@gmail.com>
2026-05-15 01:04:54 +0800

    fix: point model path to public/model folder

--- src/main.js
+++ src/main.js
@@ -9,8 +9,8 @@ async function init() {
         resizeTo: window
     });
 
-    // 自動抓取剛才上傳的中文檔名模型
-    const model = await Live2DModel.from("精細模型.model3.json");
+    // 指向你貼圖與模型所在的正確資料夾
+    const model = await Live2DModel.from("public/model/精細模型.model3.json");
     app.stage.addChild(model);
     
     model.x = 0;
